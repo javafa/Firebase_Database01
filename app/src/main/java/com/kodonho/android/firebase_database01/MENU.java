0738dff2be545c60c5f6a5da0185838087ed328d
javafa <nigne2016@gmail.com>
2016-11-01 16:01:18 +0900

    최초등록

--- app/src/main/java/com/kodonho/android/firebase_database01/MENU.java
+++ app/src/main/java/com/kodonho/android/firebase_database01/MENU.java
@@ -12,39 +12,32 @@ public class MENU
 
     private String MENU_IMAGE;
 
-    public String getMENU_NAME ()
-    {
+    public String getMENU_NAME (){
         return MENU_NAME;
     }
 
-    public void setMENU_NAME (String MENU_NAME)
-    {
+    public void setMENU_NAME (String MENU_NAME){
         this.MENU_NAME = MENU_NAME;
     }
 
-    public String getMENU_PRICE ()
-    {
+    public String getMENU_PRICE (){
         return MENU_PRICE;
     }
 
-    public void setMENU_PRICE (String MENU_PRICE)
-    {
+    public void setMENU_PRICE (String MENU_PRICE){
         this.MENU_PRICE = MENU_PRICE;
     }
 
-    public String getMENU_IMAGE ()
-    {
+    public String getMENU_IMAGE (){
         return MENU_IMAGE;
     }
 
-    public void setMENU_IMAGE (String MENU_IMAGE)
-    {
+    public void setMENU_IMAGE (String MENU_IMAGE){
         this.MENU_IMAGE = MENU_IMAGE;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString(){
         return "ClassPojo [MENU_NAME = "+MENU_NAME+", MENU_PRICE = "+MENU_PRICE+", MENU_IMAGE = "+MENU_IMAGE+"]";
     }
 }
--- app/src/main/java/com/kodonho/android/firebase_database01/MainActivity.java
+++ app/src/main/java/com/kodonho/android/firebase_database01/MainActivity.java
@@ -1,12 +1,27 @@
 package com.kodonho.android.firebase_database01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static ArrayList<Branch> branches;
+
+    FirebaseDatabase database;
+    DatabaseReference chickenRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        database = FirebaseDatabase.getInstance();
+        chickenRef = database.getReference("CHICKENSTORE");
     }
+
+
 }
